fc2c8ff7df7e8a708d32d77dc75949fcb4f0fec3
Rod20 <nimrod2560@gmail.com>
2021-07-13 16:23:55 -0400

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'paisesApp';
+
+  constructor(
+    private router: Router,
+    private gtmService: GoogleTagManagerService,
+  ) {}
+
+  customEvent() {
+
+    // push GTM data layer with a custom event
+    const gtmTag = {
+      event: 'button-click',
+      data: 'my-custom-event',
+    };
+    this.gtmService.pushTag(gtmTag);
+
+    alert('this is a custom event');
+  }
+
+  ngOnInit() {
+    // push GTM data layer for every visited page
+    this.router.events.forEach(item => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: 'page',
+          pageName: item.url
+        };
+
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { GoogleTagManagerModule } from 'angular-google-tag-manager';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaisModule,
+    SharedModule,
+    GoogleTagManagerModule.forRoot({
+      id: 'GTM-TM2B5C4',
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: 'googleTagManagerId', useValue: 'GTM-TM2B5C4' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
